9b8c4a3b12c55f01cb042e8539db142788c7697a
Jerry Chang <jerry1215jerry@gmail.com>
2015-12-04 13:41:21 -0800

    first commit

--- LLDB Command Test/ViewController.m
+++ LLDB Command Test/ViewController.m
@@ -17,11 +17,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *aString = @"a string";
+    int i = 99;
+    NSLog(@"#1: %d", i);
+    NSLog(@"#2: %d", i);
+}
+
+-(void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:YES];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+static BOOL isEven(int i){
+    if (i %2 == 0){
+        NSLog(@"%d is even!", i);
+        return YES;
+    }
+    NSLog(@"%d is odd!", i);
+    return NO;
 }
 
 @end
